32cbeab4c233280ee6f9aeef9a06a924aca3e7ef
jeremie bitancor <bitancor.jeremie@gmail.com>
2021-06-12 06:37:03 +0800

    Initial Commit

---
+++ Friends/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Friends
+//
+//  Created by jeremie bitancor on 6/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
